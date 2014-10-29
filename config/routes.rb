2e31be5411811244af1590faadb078f52e267033
alex <aturjan@yahoo.com>
2014-10-29 19:15:36 +0100

    Initialize repository

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+  get 'signup' => 'users#new'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+
+  root 'static_pages#home'
+
+  resources :users
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
