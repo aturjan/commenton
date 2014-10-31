c8bb220e17c4349d1e4a66f8659a8bc9cd32e05b
alex <aturjan@yahoo.com>
2014-10-31 18:45:56 +0100

    Use SSL and Unicorn in production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,34 @@ class UsersController < ApplicationController
   end
 
   def new
- 	@user = User.new
+    puts params
+ 	if params[:commit] == "Log in"
+     user = User.find_by(email: params[:users][:email].downcase)
+    if user && user.authenticate(params[:users][:password])
+      # Log the user in and redirect to the user's show page.
+      log_in user
+      redirect_to user
+    else
+        flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  else  
+    @user = User.new
+  end  
+
   end
 
+
+  def destroy
+    log_out
+    redirect_to signup_url
+  end
 
  def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the CommentOn!"
       redirect_to @user
     else
       render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   get 'signup' => 'users#new'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
 
-  root 'static_pages#home'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'users#destroy'
+
+  root 'users#new'
 
   resources :users
 
